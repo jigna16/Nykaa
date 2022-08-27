148f57cd32fcadef99f9c3a0a2f78113df2d5b7c
preeti singh <72785433+preeticodeholic@users.noreply.github.com>
2022-08-27 11:36:13 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,7 +1,28 @@
-function cart(){
-    // Complete to show an alert with your product
-    alert("Add your product");
-}
+
+    function searchData() {
+        let query = document.querySelector("#search").value;
+        if (query === "face primer"){
+            window.location.href = "./face_primer.html";
+        }
+        else if (query === "concealer"){
+            window.location.href = "./concealer.html";
+        }
+    }
+
+    let id;
+    function debounce (func,delay) {
+        if (id) {
+            clearTimeout(id);
+        }
+        id = setTimeout(function () {
+            func();
+        },delay)
+    }
+
+    function cart(){
+        // Complete to show an alert with your product
+        alert("Add your product");
+    }
 
 
 
